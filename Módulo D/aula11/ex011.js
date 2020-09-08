1a6ad7a9ff9de5f2fc7c7c0751b4f1e381d2d8ca
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2020-09-08 16:38:25 -0300

    Update ex011.js

--- Módulo D/aula11/ex011.js
+++ Módulo D/aula11/ex011.js
@@ -3,5 +3,5 @@ console.log(`Vivendo em ${pais}`)
 if (pais == 'Brasil') {
     console.log('Você é Brasilheiro')
 } else {
-    console.log('Você é Estranjgeiro')
+    console.log('Você é Estrangeiro')
 }
