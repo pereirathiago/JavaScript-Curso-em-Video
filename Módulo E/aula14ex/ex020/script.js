3e7a30976ce18522cf063518ecd4c92ace312ee4
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-09-14 13:50:14 -0300

    tabuada

--- Módulo E/aula14ex/ex020/script.js
+++ Módulo E/aula14ex/ex020/script.js
@@ -1,9 +1,24 @@
 function calcular() {
-    var txtnum = document.getElementById('txtnum')
-    var res = document.getElementById('res')
-    var num = Number(txtnum.value)
-    for (var c = 1; c <= 10; c++) {
-        r = num * c
-        res.innerHTML += `${num} X ${c} = ${r} <br>` 
+    let txtnum = document.getElementById('txtnum')
+    let tab = document.getElementById('seltab')
+    if (txtnum.value.length == 0) {
+        alert('Por favor, digite um numero')
+    } else {
+        let num = Number(txtnum.value)
+        // for (let c = 1; c <= 10; c++) {
+        //     r = num * c
+        //     let item = document.createelement('option')
+        //     item.text = `${num} X ${c} = ${r}`
+        //     tab.appendChild(item)
+        // } 
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} X ${c} = ${num*c}`
+            item.value = `tab{c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
