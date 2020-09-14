e5857886b3756153e43b3891b4b555c2e5cdc24f
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-09-14 13:13:18 -0300

    Update script.js

--- Módulo D/aula14ex/ex019/script.js
+++ Módulo D/aula14ex/ex019/script.js
@@ -5,15 +5,26 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados!')
+        res.innerHTML = `Impossível contar!`
+        // alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
